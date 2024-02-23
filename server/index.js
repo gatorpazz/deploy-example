202d0377d608de4f8b5c01ca2f30fced226f7ddc
Michael Pascuzzi <gatorpazz@gmail.com>
2024-02-22 22:29:04 -0500

    code

--- server/index.js
+++ server/index.js
@@ -6,8 +6,10 @@ const path = require('path');
 
 const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/acme_notes_db');
 // static routes here (you only need these for deployment)
-
+app.use(express.static(path.join(__dirname, '../client/dist')));
 // app routes here
+app.get('/', (req, res)=> res.sendFile(path.join(__dirname, '../client/dist/index.html'))). 
+
 app.get('/api/notes', async (req, res, next) => {
   try {
     const SQL = `SELECT * FROM notes;`
