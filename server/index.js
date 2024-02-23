193fee906fc082b9fe3eaf2d3c3f5e262a194770
Michael Pascuzzi <gatorpazz@gmail.com>
2024-02-22 22:24:10 -0500

    code

---
+++ server/index.js
@@ -0,0 +1,49 @@
+// imports here for express and pg
+const express = require('express');
+const app = express();
+const pg = require('pg');
+const path = require('path');
+
+const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/acme_notes_db');
+// static routes here (you only need these for deployment)
+
+// app routes here
+app.get('/api/notes', async (req, res, next) => {
+  try {
+    const SQL = `SELECT * FROM notes;`
+    const response = await client.query(SQL);
+    res.send(response.rows);
+  } catch (err) {
+    next(err);
+  }
+})
+
+// create your init function
+const init = async() => {
+  await client.connect();
+
+  const SQL = `
+    DROP TABLE IF EXISTS notes;
+
+    CREATE TABLE notes(
+      id SERIAL PRIMARY KEY,
+      txt VARCHAR(255),
+      starred BOOLEAN DEFAULT FALSE
+    );
+
+    INSERT INTO notes(txt, starred) VALUES('learn express', false);
+    INSERT INTO notes(txt, starred) VALUES('write SQL queries', true);
+    INSERT INTO notes(txt) VALUES('create routes');
+  `;
+
+  await client.query(SQL);
+  console.log('data seeded');
+
+  const PORT = process.env.PORT || 3000;
+
+  app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`)
+  })
+}
+// init function invocation
+init();
